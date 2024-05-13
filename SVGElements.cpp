b68d2c7f9145caecd897b76b32ef776e53bc32ae
Dead4ever07 <168198230+Dead4ever07@users.noreply.github.com>
2024-05-13 14:35:00 +0100

    Add files via upload

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -1,23 +1,230 @@
 #include "SVGElements.hpp"
+#include <iostream>
 
 namespace svg
 {
-    // These must be defined!
-    SVGElement::SVGElement() {}
-    SVGElement::~SVGElement() {}
+    SVGElement::SVGElement()
+    {
+    }
+    SVGElement::~SVGElement() 
+    {
+    }
+    std::vector<SVGElement*>& SVGElement::get_elements()
+    {
+        std::vector<SVGElement*> v;
+        std::vector<SVGElement*>&  a= v; 
+        return a;
+    }
 
     // Ellipse (initial code provided)
     Ellipse::Ellipse(const Color &fill,
                      const Point &center,
                      const Point &radius)
-        : fill(fill), center(center), radius(radius)
+        :fill_(fill), center_(center), radius_(radius) 
     {
     }
     void Ellipse::draw(PNGImage &img) const
     {
-        img.draw_ellipse(center, radius, fill);
+        img.draw_ellipse(center_, radius_, fill_);
     }
-    // @todo provide the implementation of SVGElement derived classes
-    // HERE -->
+    void Ellipse::translate(const Point&t)
+    {
+        center_ = this->center_.translate(t);
+        
+    }
+    void Ellipse::rotate(const Point &origin, int degrees) 
+    {
+        center_ = this->center_.rotate(origin,degrees);
+    }
+    void Ellipse::scale(const Point&origin, int v) 
+    {
+        center_ = this->center_.scale(origin,v);
+        radius_ = this->radius_.scale({0,0},v);
+    }
+    Ellipse* Ellipse::get_clone()
+    {
+        Ellipse* ellipse_clone = new Ellipse(this->fill_, this->center_, this->radius_) ;
+        return ellipse_clone;
+    }
+
+
+    Circle::Circle(const Color &fill,
+                   const Point &center,
+                   const int &radius
+                   )
+        :Ellipse(fill,center, {radius,radius}){};
+
+    Circle* Circle::get_clone()
+    {
+        Circle* clone_circle = new Circle(this->fill_,this->center_,this->radius_.x);
+        return clone_circle;
+    }
+
+    Polyline::Polyline(const std::vector<Point> &points,
+                       const Color &stroke )
+       : points_(points), stroke_(stroke)
+    {
+    }
+    void Polyline::draw(PNGImage &img) const
+    {
+        size_t len = points_.size();
+        for (size_t i = 0; i < len-1; i++) 
+        {
+            img.draw_line(points_[i], points_[i+1], stroke_);
+        }
+    }
+
+    void Polyline::translate( const Point&t)
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.translate(t);
+        }
+    }
+    void Polyline::rotate( const Point &origin, int degrees)
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.rotate(origin,degrees);
+        }
+    }
+    void Polyline::scale(const Point&origin, int v) 
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.scale(origin,v);
+        }
+    }
+    Polyline* Polyline::get_clone()
+    {
+        Polyline* clone_polyline = new Polyline(this->points_,this->stroke_);
+        return clone_polyline;
+    }
+
+
+
+    Line::Line(const svg::Point &first,
+               const svg::Point &second,
+               const svg::Color &stroke)
+       : Polyline(std::vector<Point> {first, second}, stroke)
+    {
+    }
+
+    Line* Line::get_clone()
+    {
+        Line *clone_line = new Line(this->points_[0],this->points_[1],this->stroke_);
+        return clone_line;
+    }
+
+    Polygon::Polygon(const std::vector<Point> &points,
+                     const svg::Color &fill)
+         :points_(points), fill_(fill)
+    {
+    }
+    void Polygon::draw(PNGImage &img) const
+    {
+        img.draw_polygon(points_, fill_);
+    }
+    void Polygon::translate(const Point&t)
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.translate(t);
+        }
+    }
+    void Polygon::rotate(const Point &origin, int degrees)
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.rotate(origin,degrees);
+        }
+    }
+    void Polygon::scale(const Point&origin, int v) 
+    {
+        for(Point& p:this->points_)
+        {
+            p = p.scale(origin,v);
+        }
+    }
+
+    Polygon* Polygon::get_clone()
+    {
+        Polygon* clone_polygon = new Polygon(this->points_, this->fill_);
+        return clone_polygon;
+    }
+
+
+
+    Rectangle::Rectangle(const svg::Point &point,
+                         const int &width,
+                         const int &height,
+                         const svg::Color &fill)
+         : Polygon(std::vector<Point> {point, {point.x + width-1, point.y}, {point.x + width-1, point.y + height-1}, {point.x, point.y + height-1}}, fill) {}
+    
+    Rectangle* Rectangle::get_clone()
+    {
+        Rectangle* clone_rectangle = new Rectangle(this->points_[0], this->points_[2].x-this->points_[0].x+1,this->points_[2].y-this->points_[0].y+1, this->fill_);
+        return clone_rectangle;
+    }
+    
+    Group::Group(){}
+
+
+
+    Group::~Group()
+    {
+        for (SVGElement* child : this->children_)
+        {
+            delete child;
+        }
+    }
+    
+    std::vector<SVGElement*>& Group::get_elements()
+    {
+        return children_;
+    }
+
+    void Group::draw(PNGImage &img) const 
+    {
+        for(SVGElement* child : this->children_ )
+        {
+            child->draw(img);
+           
+        }
+    }
+
+    void Group::translate(const Point& t)
+    {
+        for(SVGElement* child : this->children_ )
+        {
+            child->translate(t);
+        }
+    }
+    void Group::rotate(const Point &origin, int degrees) 
+    {
+        for(SVGElement* child : this->children_ )
+        {
+            child->rotate(origin, degrees);
+        }
+    }
+    void Group::scale(const Point&origin, int v) 
+    {
+        for(SVGElement* child : this->children_ )
+        {
+            child->scale(origin, v);
+        }
+    }
+
+    Group* Group::get_clone()
+    {
+
+        Group* clone_group = new Group();
+        for(SVGElement* c : this->children_){
+            clone_group->get_elements().push_back(c->get_clone());
+        }
+        return clone_group;
+    }
 
 }
+
+
--- SVGElements.hpp
+++ SVGElements.hpp
@@ -12,9 +12,22 @@ namespace svg
     {
 
     public:
+        //Constructor that creats a SVGelement
         SVGElement();
+        //Destructor that destrois a SVGelement
+        //It will only be override in the Group subclass
         virtual ~SVGElement();
+        //!Draw function, it will be ouveride by every subclass
+        //!@param img Image File name
         virtual void draw(PNGImage &img) const = 0;
+        //!Draw
+        virtual void translate(const Point&t) = 0;        
+        virtual void rotate(const Point &origin, int degrees) = 0;
+        virtual void scale(const Point&origin, int v) = 0;
+        virtual std::vector<SVGElement*>& get_elements();
+        virtual SVGElement* get_clone() = 0;
+
+
     };
 
     // Declaration of namespace functions
@@ -32,13 +45,98 @@ namespace svg
     public:
         Ellipse(const Color &fill, const Point &center, const Point &radius);
         void draw(PNGImage &img) const override;
+        void translate( const Point&t)  override;
+        void rotate( const Point &origin, int degrees) override;
+        void scale(const Point&origin, int v) override;
+        virtual Ellipse* get_clone() override;
 
-    private:
-        Color fill;
-        Point center;
-        Point radius;
+
+    protected:
+        Color fill_;
+        Point center_;
+        Point radius_;
+    };
+
+    class Circle : public Ellipse
+    {
+    public:
+        Circle(const Color &fill, const Point &center, const int &radius);
+        Circle* get_clone() override;
     };
 
 
-}
+    class Polyline : public SVGElement{
+    public:
+        Polyline(const std::vector<Point> &points, const Color &stroke);
+        void draw(PNGImage &img) const override;
+        void translate(const Point&t)  override;
+        void rotate(const Point &origin, int degrees)  override;
+        void scale(const Point&origin, int v) override;
+        virtual Polyline* get_clone() override;
+        
+
+    protected:
+        std::vector<Point> points_;
+        Color stroke_;
+
+    };
+
+    class Line : public Polyline{
+    public:
+        Line(const Point &first, const Point &second, const Color &stroke);
+        Line* get_clone() override;
+
+    };
+
+
+    class Polygon : public SVGElement
+    {
+    public:
+        Polygon(const std::vector<Point> &points, const Color &fill);
+        void draw(PNGImage &img) const override;
+        void translate(const Point&t) override;
+        void rotate(const Point &origin, int degrees) override;
+        void scale(const Point&origin, int v) override;
+        virtual Polygon* get_clone() override;
+        
+
+    protected:
+        std::vector<Point> points_;
+        Color fill_;
+    };
+
+    class Rectangle : public Polygon
+    {
+    public:
+        Rectangle(const Point &point, const int &width, const int &height, const Color &fill);
+        Rectangle* get_clone() override;
+
+    };
+
+    class Group : public SVGElement 
+    {
+    public:
+
+        Group();
+        ~Group();
+        std::vector<SVGElement*>& get_elements();
+        void draw(PNGImage &img) const override;
+        void translate(const Point& t) override;
+        void rotate(const Point &origin, int degrees)  override;
+        void scale(const Point&origin, int v) override;
+        Group* get_clone() override;
+        
+
+    protected:
+        std::vector<SVGElement*> children_;
+    };
+    
+
+
+
+
+
+
+};
 #endif
+
